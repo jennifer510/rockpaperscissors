96fd0cccb1334d97bf72d33242ae66a792e2f017
jennifer123 <jenifferamezquita@ccpaedu.com>
2018-10-25 01:01:18 +0000

    added new variables

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
+
+
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 //GLOBAL VARIABLES
 /* global $ */
-
-
+var userChoice = "";
+var computerChoice = "";
+var winner = "";
+var randomNumber = Math.random();
 
 // DOCUMENT READY FUNCTION BELOW
-$("#buttont").click(function(){
-    $("userChoice").text($("#input").val()
-})
 
-$("#choices").click(function(){
-    $("computerChoice").text($("#input").val()
-})
+$("button").click(function() {
+  $("#userChoice").text($("#input").val());
+});
+
+$("#choices").click (function(){
+    $("#computerChoice").text ($("#input").val());
+     if (randomNumber > 0.6)
+    {
+        computerChoice = "rock";
+    }
+    else if(randomChoice =)
+    
+    
+});
